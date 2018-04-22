9426b3b1dcb714c7cb078c1cdea65df87b6d54e3
David Darke <david@atomicsmash.co.uk>
2018-04-22 15:17:56 +0100

    Created MacilChimp key generation

--- ultimate-mailchimp-plugin.php
+++ ultimate-mailchimp-plugin.php
@@ -20,6 +20,7 @@ use \DrewM\MailChimp\Batch;
 
 require __DIR__ . '/vendor/autoload.php';
 
+//ASTODO Need to add logging!
 // use Monolog\Logger;
 // use Monolog\Handler\StreamHandler;
 //
@@ -215,16 +216,31 @@ class UltimateMailChimpPlugin {
 
     public function webhook() {
 
+        //ASTODO need to check if there is a key set
         return "You posted to the webhook!";
 
     }
 
     public function generate_webhook_url() {
 
-        $webhook_url_key = get_option( 'webhook_url_key' );
+        $webhook_key = get_option( 'webhook_url_key' );
 
-        // if wehoook...
+        if( $webhook_key == "" ){
+            WP_CLI::confirm( "No webhook key is currently saved. Would you like to generate one?" );
+        }else{
+            WP_CLI::line( "Your current webhook url is: " . get_bloginfo('url') . "/wp-json/ultimate-mailchimp/v1/?key=" . $webhook_key );
 
+            WP_CLI::confirm( "Would you like to regenerate it?" );
+        }
+
+        // The key is only regenerated is a 'Y' is supplied to a previous question
+        $new_key = uniqid();
+
+        // Update the option, but don't autoload it
+        update_option( 'webhook_url_key', $new_key, 0 );
+
+        //ASTODO get the webhook url into a global var
+        WP_CLI::line( "Your NEW webhook url is: " . WP_CLI::colorize( "%G" . get_bloginfo('url') . "/wp-json/ultimate-mailchimp/v1/?key=" . $new_key . "%n" ));
 
     }
 
